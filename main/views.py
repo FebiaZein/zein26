e18323c0ebf0e7d6111b759875c2515005d7d256
zein26 <febiaaziza@gmail.com>
2021-12-22 10:45:28 +0700

    project

--- main/views.py
+++ main/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'main/home.html')
+    if request.method == 'POST':
+        masuk1= int(request.POST['Tabungan Awal'])
+        masuk2= int(request.POST['Bunga'])
+        masuk3= int(request.POST['Lama'])
+        keluar=((masuk2/100)*(masuk3/12)*masuk1)+masuk1
+        context = {'keluar': keluar,
+                   }
+        return render(request, 'main/home.html', context)
+
+    else:
+        return render(request, 'main/home.html', {})
+
